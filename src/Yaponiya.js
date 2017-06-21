1bf705de18b22f5d85b57c84ccfdf617839b347a
Enrico B <rflnrclws@protonmail.com>
2017-06-21 15:15:44 -0400

    Finish basic routing structure

--- src/Yaponiya.js
+++ src/Yaponiya.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
+import Calendar from './components/Calendar';
 import KanjiStore from './components/KanjiStore';
 import Header from './components/Header';
 
 const Yaponiya = () => (
   <div>
-    <Header />
-    <KanjiStore />
+    <nav>
+      <Header />
+    </nav>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={KanjiStore} />
+        <Route path="/calendar" component={Calendar} />
+      </Switch>
+    </Router>
   </div>
 );
 
