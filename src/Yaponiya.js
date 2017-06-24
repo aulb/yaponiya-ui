3c5a7315679b0e11e62ef3a676629e99d848ce37
Enrico B <rflnrclws@protonmail.com>
2017-06-24 19:44:51 -0400

    Use Switch

--- src/Yaponiya.js
+++ src/Yaponiya.js
@@ -2,28 +2,32 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
   withRouter,
 } from 'react-router-dom';
+
 import KanjiStore from './components/KanjiStore';
 import KanjiPage from './components/KanjiPage';
 import Calendar from './components/Calendar';
 import Header from './components/Header';
 
-// Endow Header with routing info
 const HeaderRouter = withRouter(Header);
 
 const Yaponiya = () => (
-  <div>
-    <Router>
-      <div>
-        <HeaderRouter />
+  <Router>
+    <div>
+      <HeaderRouter />
+      <Switch>
         <Route exact path="/" component={KanjiStore} />
         <Route path="/kanji/:character" component={KanjiPage} />
-        <Route path="/data/:year/:month" component={KanjiStore} />
+        <Route path="/:year/:month" component={KanjiStore} />
         <Route path="/calendar" component={Calendar} />
-      </div>
-    </Router>
-  </div>
+      </Switch>
+
+
+
+    </div>
+  </Router>
 );
 
 export default Yaponiya;
