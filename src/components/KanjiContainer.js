1bdb99e9266c06ba728fc4c41bab8afc33c1ccd8
Enrico B <rflnrclws@protonmail.com>
2017-07-06 13:55:56 -0400

    Refactor KanjiContainer into separate functions for calculating color

--- src/components/KanjiContainer.js
+++ src/components/KanjiContainer.js
@@ -3,6 +3,8 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import KanjiCharacter from './KanjiCharacter';
 import { palette } from '../helpers/palette';
 
+// The top-N kanji that are statistically significant
+const CUTOFF_INDEX = 50;
 const SEQ_PALETTE = palette('cb-Blues', 9).slice(1);
 // const SEQ_PALETTE2 = [
   // 'F7FCFD',
@@ -16,26 +18,33 @@ const SEQ_PALETTE = palette('cb-Blues', 9).slice(1);
   // '4D004B',
 // ];
 
-function KanjiContainer({ kanjiList }) {
-  // Grab the largest kanji count to make a ratio against
-  const counts = kanjiList.map(kanji => kanji.count)
-    .filter(count => count)
+function getSortedCounts(kanjiList) {
+  return kanjiList.map(kanji => kanji.get('count'))
     .sort((a, b) => -(a - b)); // largest num first
-  const cutoffCountIndex = 50;
-  const significantKanji = counts.slice(0, cutoffCountIndex);
-  const otherKanji = counts.slice(cutoffCountIndex);
-  const largestCount = otherKanji[0];
-  const kanjiCharacters = kanjiList.map((kanji) => {
+    // .filter(count => count)
+}
+
+function calculateRatio(count, largestCount) {
+  return count / largestCount > 1
+    ? 1
+    : count / largestCount;
+}
+
+function getPaletteIndex(countRatio) {
+  return Math.floor(countRatio * (SEQ_PALETTE.length - 1));
+}
+
+// Set up
+function kanjiMapClosure(largestCount, mostUsedKanji) {
+  return (kanji) => {
     // Avoid nulls
     const count = kanji.get('count') || 0;
-    const countRatio = count / largestCount > 1
-      ? 1
-      : count / largestCount;
+    const countRatio = calculateRatio(count, largestCount);
     // Map percentage count to palette
-    const paletteIndex = Math.floor(countRatio * (SEQ_PALETTE.length - 1));
-    const link = `/kanji/${kanji.get('id')}`;
+    const paletteIndex = getPaletteIndex(countRatio);
+    // const link = `/kanji/${kanji.get('id')}`;
     // Check if kanji has a significant count, grab
-    const bgColorHex = significantKanji.includes(count)
+    const bgColorHex = mostUsedKanji.includes(count)
       ? '4D004B'
       : SEQ_PALETTE[paletteIndex];
     const backgroundColor = `#${bgColorHex}`;
@@ -48,19 +57,27 @@ function KanjiContainer({ kanjiList }) {
         count={count}
         fontColor={fontColor}
         backgroundColor={backgroundColor}
-        link={link}
         key={kanji.get('id')}
       >
         {String(kanji.get('id'))}
       </KanjiCharacter>
     );
-  });
+  };
+}
 
-// h2 - debugging
+function KanjiContainer({ kanjiList }) {
+  // Grab the largest kanji count to make a ratio against
+  const counts = getSortedCounts(kanjiList);
+  const mostUsedKanji = counts.slice(0, CUTOFF_INDEX);
+  const lessUsedKanji = counts.slice(CUTOFF_INDEX);
+  // Only the largest count from the lessUsed otherwise
+  // the color gradient won't be obvious
+  const largestCount = lessUsedKanji[0];
+  const kanjiMapFn = kanjiMapClosure(largestCount, mostUsedKanji);
+
   return (
     <div>
-      <h2 style={{color: 'red'}}>Largest count: {largestCount} | Sig. Kanji {significantKanji.length} | First Count: {counts[0]} | countslength: {counts.length}</h2>
-      { kanjiCharacters }
+      { kanjiList.map(kanjiMapFn) }
     </div>
   );
 }
