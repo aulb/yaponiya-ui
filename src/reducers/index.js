8061b0038c76d73539c2204ec331ce4c7385988e
Enrico B <rflnrclws@protonmail.com>
2017-07-10 17:34:38 -0400

    Define reducers for action types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,32 @@
-export const kanjis = (state = {}, action) => {
+import { Map } from 'immutable';
+// import { combineReducers } from 'redux';
+import * as types from '../actions/actionTypes';
+import { kanjiFactory } from '../helpers/utils';
+
+export default function kanjiReducer(state = Map({
+  fetched: false,
+  isLoading: false,
+  kanjis: kanjiFactory(2136),
+  error: false,
+}), action = null) {
   switch (action.type) {
-    case 'UPDATE_COUNT':
-      const newCount = state.get(action.id).set(action.count);
-      return state.set(action.id, newCount);
+    case types.RECEIVE_ERROR:
+      return state.set('error', true);
+    case types.RECEIVE_COUNTS: {
+      const keysToKanji = (acc, kanji) => {
+        const newCount = action.response[kanji];
+        return acc.set(kanji, newCount);
+      };
+      const newKanjis = Object.keys(action.response)
+        .filter(key => state.get('kanjis').get(key))
+        .reduce(keysToKanji, state.get('kanjis'));
+      return state
+        .set('kanjis', newKanjis)
+        .set('isLoading', false)
+        .set('fetched', true);
+    } case types.REQUEST_DATA:
+      return state.set('isLoading', true);
     default:
       return state;
   }
-};
+}
