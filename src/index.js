39a625437c7e30aa9538c2a470408cf63933eedf
Enrico B <rflnrclws@protonmail.com>
2017-07-10 17:33:52 -0400

    Remove useless imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Yaponiya from './Yaponiya';
 import registerServiceWorker from './registerServiceWorker';
-import { kanjiFactory } from './helpers/utils';
-import kanjis from './store/reducers';
 import './styles/index.css';
 
 ReactDOM.render(<Yaponiya />, document.getElementById('root'));
