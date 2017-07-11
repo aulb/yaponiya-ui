9f4b76a36685661917d4813e50a35e028df08609
Enrico B <rflnrclws@protonmail.com>
2017-07-11 12:27:30 -0400

    Connect KanjiCatalogContainer to store

--- src/containers/KanjiCatalogContainer.js
+++ src/containers/KanjiCatalogContainer.js
@@ -1,129 +1,45 @@
-import React, { Component } from 'react';
-import Immutable from 'immutable';
-import io from 'socket.io-client';
-import KanjiCatalog from '../components/KanjiCatalog';
-import Options from '../components/Options';
-import { kanjiFactory } from '../helpers/utils';
+import { connect } from 'react-redux';
+import { fetchData, updateSort } from '../actions';
 import { OPTIONS } from '../helpers/constants';
+import KanjiCatalog from '../components/KanjiCatalog';
 
-const socket = io('http://reblws.me:8080');
+function sortBy(sort) {
+  const descending = (a, b) => -(a[sort] - b[sort]);
+  const ascending = (a, b) => a[sort] - b[sort];
+
+  return sort === OPTIONS.FREQUENCY
+    ? descending
+    : ascending;
+}
+
+function getKanjiList(kanjiMap, currentSort) {
+  return kanjiMap.keySeq()
+    .map(key => kanjiMap.get(key))
+    .toList()
+    .sort(sortBy(currentSort));
+}
 
 function mapStateToProps(state) {
   return {
-
-  }
+    kanjiList: getKanjiList(state.kanjis, state.currentSort),
+    currentSort: state.currentSort,
+  };
 }
 
-class KanjiCatalogContainer extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      currentOrder: OPTIONS.FREQUENCY,
-      /* Example kanji state object
-        [
-          {
-            id: currentChar,
-            alphabetical: ORDERS.ALPHABETICAL[i], // TODO to be deleted
-            heisig: ORDERS.HEISIG[i],
-            frequency: count, // TODO to be deleted
-            isFlash: false
-          },
-        ]
-      */
-      kanjiMap: Immutable.Map(kanjiFactory(numOfKanji)),
-      tweetFlash: [],
-    };
-    this.switchOrder = this.switchOrder.bind(this);
-    this.handleTweet = this.handleTweet.bind(this);
-    this.handleFetch = this.handleFetch.bind(this);
-  }
-
-  componentDidMount() {
-    socket.on('tweet', this.handleTweet);
-
-    const kanjiReducer = response => (acc, key) => {
-      const newCount = response[key];
-      const newKanjiEntry = acc.get(key).set('count', newCount);
-      return acc.set(key, newKanjiEntry);
-    };
-
-    const fetchReducer = kanjiMap => (
-      (response) => {
-        const filteredKeys = Object.keys(response).filter(key => kanjiMap.get(key));
-        const reduceKeysToKanji = kanjiReducer(response);
-        return filteredKeys.reduce(reduceKeysToKanji, kanjiMap);
-      }
-    );
-
-    const updateKanjiMap = fetchReducer(this.state.kanjiMap);
-
-    fetch('http://reblws.me:5000/api/data/nhk')
-      .then(response => response.json())
-      .then(updateKanjiMap)
-      .then(this.handleFetch);
-  }
-
-  handleFetch(kanjiMap) {
-    this.setState({ kanjiMap });
-  }
-
-  handleTweet(tweet) {
-    this.setState({ tweetFlash: tweet });
-  }
-
-  switchOrder(event) {
-    // // Get new order from the event
-    const nextOrder = event.target.value;
-    this.setState({
-      currentOrder: nextOrder,
-    });
-  }
-
-  get kanjiList() {
-    // Sort the kanjiList in order
-    const kanjiMap = this.state.kanjiMap;
-    const kanjiList = kanjiMap.keySeq()
-      .map(key => Immutable.Map({
-        id: [key],
-        count: kanjiMap.get(key).get('count'),
-        alphabetical: kanjiMap.get(key).get('alphabetical'),
-        heisig: kanjiMap.get(key).get('heisig'),
-        nhk: kanjiMap.get(key).get('count'),
-        isFlash: this.state.tweetFlash.includes(key),
-      })).toList();
-    const order = this.state.currentOrder.toLowerCase();
-    return kanjiList.sort((a, b) => {
-      if (!a.get(order)) return 1;
-      if (!b.get(order)) return 0;
-
-      const descending = -(a.get(order) - b.get(order));
-      const ascending = a.get(order) - b.get(order);
-
-      return order === 'nhk' // TODO revamp
-        ? descending
-        : ascending;
-    });
-  }
-
-  render() {
-    /* DEBUG
-    <header>
-      <h1>{this.state.tweetFlash}</h1>
-    </header>
-    */
-    return (
-      <div>
-        <Options
-          currentOrder={this.state.currentOrder}
-          switchOrder={this.switchOrder}
-          possibleOptions={OPTIONS}
-        />
-        <KanjiCatalog
-          kanjiList={this.kanjiList}
-        />
-      </div>
-    );
-  }
+function mapDispatchToProps(dispatch) {
+  return {
+    updateSort: (newSort) => {
+      dispatch(updateSort(newSort));
+    },
+    fetchData: (url) => {
+      dispatch(fetchData(url));
+    },
+  };
 }
+
+const KanjiCatalogContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(KanjiCatalog);
 
 export default KanjiCatalogContainer;
